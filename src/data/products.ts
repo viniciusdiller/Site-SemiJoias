a0d129734ac6f57083573d911034fb5b4c4d52dc
Vinícius Diller <viniciusdiller@hotmail.com>
2026-02-09 22:08:10 -0300

    feat: Adiciona tipos TypeScript e interfaces do projeto

---
+++ src/data/products.ts
@@ -0,0 +1,61 @@
+import { Product } from '../types';
+
+// Dados de exemplo - substitua com seus produtos reais
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Anel Dourado Clássico',
+    category: 'aneis',
+    price: 89.90,
+    image: '/assets/images/anel1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Brinco de Argola',
+    category: 'brincos',
+    price: 79.90,
+    image: '/assets/images/brinco1.jpg'
+  },
+  {
+    id: 3,
+    name: 'Colar Delicado',
+    category: 'colares',
+    price: 129.90,
+    image: '/assets/images/colar1.jpg'
+  },
+  {
+    id: 4,
+    name: 'Pulseira Elegante',
+    category: 'pulseiras',
+    price: 99.90,
+    image: '/assets/images/pulseira1.jpg'
+  },
+  {
+    id: 5,
+    name: 'Conjunto Premium',
+    category: 'conjunto',
+    price: 249.90,
+    image: '/assets/images/conjunto1.jpg'
+  },
+  {
+    id: 6,
+    name: 'Anel com Pedra',
+    category: 'aneis',
+    price: 119.90,
+    image: '/assets/images/anel2.jpg'
+  },
+  {
+    id: 7,
+    name: 'Brinco Gota',
+    category: 'brincos',
+    price: 89.90,
+    image: '/assets/images/brinco2.jpg'
+  },
+  {
+    id: 8,
+    name: 'Colar Longo',
+    category: 'colares',
+    price: 149.90,
+    image: '/assets/images/colar2.jpg'
+  }
+];
